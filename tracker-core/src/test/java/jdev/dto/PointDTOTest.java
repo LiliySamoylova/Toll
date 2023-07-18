b34b0298d38f18a65230aa518db7c5c4fc132858
Aspire3 <samoilovali100@gmail.com>
2023-07-18 12:48:29 +0700

    first commit

---
+++ tracker-core/src/test/java/jdev/dto/PointDTOTest.java
@@ -0,0 +1,26 @@
+package jdev.dto;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class PointDTOTest {
+
+    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\",\"time\":1489900897458}";
+    private String autoId = "o567gfd";
+
+    @Test
+    public void toJson() throws Exception {
+        PointDTO point = new PointDTO();
+        point.setLat(56);
+        point.setLon(74);
+        point.setAutoId("o567gfd");
+        point.setTime(System.currentTimeMillis());
+        assertTrue(point.toJson().contains("\"lat\":56"));
+        assertTrue(point.toJson().contains("\"time\":"));
+        System.out.println(point.toJson());
+    }
+
+}
