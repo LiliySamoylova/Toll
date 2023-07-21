5cbfe9eddfc87825b8655524cdf8c034444c5862
Aspire3 <samoilovali100@gmail.com>
2023-07-21 14:53:00 +0700

    2 задание

--- server-ui/src/test/java/jdev/dto/PointDTOTest.java
+++ server-ui/src/test/java/jdev/dto/PointDTOTest.java
@@ -1,11 +1,9 @@
 package jdev.dto;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 //import org.junit.Test;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
@@ -14,7 +12,6 @@ public class PointDTOTest {
     private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\",\"time\":1489900897458}";
     private String autoId = "o567gfd";
 
-    //тест преобразования из объекта в json
     @Test
     public void toJson() throws Exception {
         PointDTO point = new PointDTO();
@@ -27,12 +24,5 @@ public class PointDTOTest {
         System.out.println(point.toJson());
     }
 
-    //тест преобразования из json в объект
-    @Test
-    public void decodeDto() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        PointDTO dto = mapper.readValue(expected, PointDTO.class);
-        assertEquals(autoId, dto.getAutoId());
-        assertEquals(1489900897458L, dto.getTime());
-    }
 }
+
