e01533426411b35ac2a78dadfd1063408b77c8bc
Aspire3 <samoilovali100@gmail.com>
2023-07-24 14:51:59 +0700

    логгирование

--- queue-fixed-rate/src/main/java/jdev/services/ScheduledQueueService.java
+++ queue-fixed-rate/src/main/java/jdev/services/ScheduledQueueService.java
@@ -7,17 +7,37 @@ import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by jdev on 26.03.2017.
- */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Service
 public class ScheduledQueueService {
 
+    private static final Logger log = LoggerFactory.getLogger(ScheduledQueueService.class);
 
     private BlockingDeque<String> queue =  new LinkedBlockingDeque<>(100);
     private int putCount;
     private long previous;
 
+    @Scheduled (fixedDelay = 2000)
+    void take() throws InterruptedException {
+        log.info("take trying!!!");
+        long current = System.currentTimeMillis();
+        log.info((current - previous) + " ScheduledQueueService.take " + queue.poll(500, TimeUnit.MILLISECONDS));
+//        System.out.println((current - previous) + " ScheduledQueueService.take " + queue.take());
+        previous = current;
+
+    }
+
+    @Scheduled (fixedDelay = 1_000)
+    void put() throws InterruptedException {
+        int i = putCount++;
+        log.info("ScheduledQueueService.put " + i);
+        queue.put("new string => " + i);
+
+    }
+
+    /*
     @Scheduled (fixedDelay = 2000)
      void take() throws InterruptedException {
         System.out.println("take trying!!!");
@@ -28,11 +48,13 @@ public class ScheduledQueueService {
 
     }
 
-    @Scheduled (fixedDelay = 1_000)
+     @Scheduled (fixedDelay = 1_000)
      void put() throws InterruptedException {
         int i = putCount++;
         System.out.println("ScheduledQueueService.put " + i);
         queue.put("new string => " + i);
 
     }
+     */
+
 }
