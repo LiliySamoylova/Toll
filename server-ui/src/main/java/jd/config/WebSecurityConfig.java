da05a166a964f5fcfbb8525247402237d69e5158
Aspire3 <samoilovali100@gmail.com>
2023-08-25 20:29:00 +0700

    Задание 6

--- server-ui/src/main/java/jd/config/WebSecurityConfig.java
+++ server-ui/src/main/java/jd/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -14,12 +15,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/").permitAll()//доступно всем пользователям
-                .antMatchers("/image").permitAll()
-                .antMatchers("/scc").permitAll()
-
-                .antMatchers("/home").authenticated() //тем, кто авторизован
-                .antMatchers("/error").authenticated()
+                .antMatchers("/css", "/image", "/css/**").permitAll() // "/css/**", "/css/homeStyle.css"
+                .antMatchers("/", "/home", "/error").authenticated()
                 .antMatchers("/routes").hasRole("CLIENT")
                 .antMatchers("/payments").hasRole("CLIENT")
                 .antMatchers("/registerClient").hasRole("MANAGER")
